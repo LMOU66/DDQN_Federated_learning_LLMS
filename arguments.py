ab092fcbaec5f0ce8cbc0ac7416986e813af0390
Mouheb Ben Nasr <148394803+LMOU66@users.noreply.github.com>
2025-05-25 16:52:51 +0100

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -9,13 +9,8 @@ class Args:
     num_rounds: int
     lora_r: int
     client_epochs: int
-    teacher_pretrain_epochs: int
-    kd_epochs: int
-    teacher_ckpt: str
     client_ckpt: str
-    teacher_data_pct: int
     client_lr: float
-    distill_lr: float
     rank: int
     data_path: str
     data_name: str
@@ -29,7 +24,6 @@ def parse_args() -> Args:
     parser.add_argument("--num_rounds", type=int, default=10)
     parser.add_argument('--lora_r', type=int, default=8)
     parser.add_argument('--client_epochs', type=int, default=5)
-    parser.add_argument('--teacher_ckpt', type=str, default="bert-base-uncased")
     parser.add_argument('--client_ckpt', type=str, default="distilbert-base-uncased")
 
     parser.add_argument('--client_lr', type=float, default=5e-5)
@@ -41,8 +35,6 @@ def parse_args() -> Args:
 
     # Parse the command-line arguments
     args = parser.parse_args()
-    if args.teacher_data_pct is None:
-        args.teacher_data_pct = 100 // (args.num_clients + 1)
 
     return Args(**vars(args))
 
